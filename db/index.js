59e0b38d382b4c567ed45c7cfcc5dcfd895e059f
petra2301 <hello@petragergely.dk>
2022-04-16 11:48:22 +0200

    db connection established

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+const { user, host, database, password, port } = require('../secrets/db_configuration');
+
+const pool = new Pool({ user, host, database, password, port });
+
+pool.query('SELECT * FROM public.norwayplaces', (err, res) => {
+    if (err) return console.log(err);
+    console.log(res);
+});
